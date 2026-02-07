338b18c1e85924c8e55854cf0cd275e0c0f05cac
feruz00 <feruzatamyradow@mail.ru>
2026-02-07 12:44:09 +0500

    fixed

--- middleware/rate-limitter.js
+++ middleware/rate-limitter.js
@@ -1,21 +1,19 @@
 const rateLimit = require('express-rate-limit');
 
 const authRateLimiter = rateLimit({
-  windowMs: 10 * 60 * 1000, // 10 minutes
-
+  windowMs: 10 * 60 * 1000,
   max: 5,
-  // 👉 Only 5 login attempts per IP in 10 minutes
-
-  message: {
-    status: 429,
-    message: 'Too many login attempts. Try again in 10 minutes.',
-  },
 
   standardHeaders: true,
   legacyHeaders: false,
 
-  // Count ONLY failed requests (VERY SMART)
-  skipSuccessfulRequests: true,
+  handler: (req, res) => {
+    console.log('BLOCKED IP:', req.ip);
+
+    res.status(429).json({
+      message: 'Too many login attempts. Try again later.',
+    });
+  },
 });
 
 module.exports = authRateLimiter;
