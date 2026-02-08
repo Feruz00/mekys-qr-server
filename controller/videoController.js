d5d59149d919aaf456d6030e8d49a743918f87d0
feruz00 <feruzatamyradow@mail.ru>
2026-02-08 15:02:05 +0500

    added streamcontroller to open by video

--- controller/videoController.js
+++ controller/videoController.js
@@ -2,6 +2,7 @@
 const { QrCodes, Files } = require('../models');
 const { catchAsync } = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
+const { streamController } = require('./streamController');
 
 const openVideoByQr = catchAsync(async (req, res, next) => {
   const { id } = req.params;
@@ -14,8 +15,12 @@ const openVideoByQr = catchAsync(async (req, res, next) => {
     return next(new AppError('Media not found', 404));
   }
 
-  console.log(qr.file.id);
-  return res.redirect(`/api/stream/${qr.file.id}?qr=${qr.id}`);
+  // console.log(qr.file.id);
+
+  req.params.id = qr.file.id;
+  req.query.qr = qr.id;
+
+  return streamController(req, res, next);
 });
 
 module.exports = { openVideoByQr };
