d5742fdc9a7b8689d477116776179fd3b7762423
feruz00 <feruzatamyradow@mail.ru>
2026-02-08 15:28:52 +0500

    nginx conf added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = require('./app');
 const db = require('./models');
 const http = require('http');
 const server = http.createServer(app);
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3006;
 
 async function startServer() {
   try {
